da53bcd7a83846a5b6e3e18d54b96cc115821b2f
Kinsley Alexander <kinsleyalexander@Kinsleys-MacBook-Air.local>
2023-06-15 22:35:31 -0400

    Fixed errors and connected to frontend

--- routes/bread.js
+++ routes/bread.js
@@ -1,14 +1,25 @@
 const router = require('express').Router()
 const {
-    getAllBread, 
+    getAllBread,
     getBreadById,
-    createBread
+    createBread,
+    updateBreadById,
+    deleteBreadById
 } = require('../controllers/bread')
 
+// GET / get all breads
 router.get('/', getAllBread)
 
-router.get(':/id', getBreadById)
+// GET /:id get bread by id
+router.get('/:id', getBreadById)
 
+// POST / create bread
 router.post('/', createBread)
+
+// PUT /:id update bread by id
+router.put('/:id', updateBreadById)
+
+// DELETE /:id delete bread by id
+router.delete('/:id', deleteBreadById)
 
 module.exports = router
